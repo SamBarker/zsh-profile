3c7c28ac21f948fd0966750ea75a4ecdc8a3e4f9
Sam Barker <sbarker@redhat.com>
2023-08-10 09:39:31 +1200

    Add updateFork alias

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -3,3 +3,4 @@ alias dropMerged='git branch --merged | egrep -v "(^\*|master|main|dev)" | xargs
 alias bundleUpdate='antibody bundle < ${MY_PROFILE:-$HOME}/zsh_plugins.txt > ${MY_PROFILE:-$HOME}/zsh_plugins.zsh'
 alias ls='ls --hyperlink=auto'
 alias mci='mvn clean install'
+alias updateFork='gco main && grhh && gf upstream && grb upstream/main && gpf!' #checkout main which tracks origin/main make sure its reset. Fetch & rebase to the latest upstream/main and push that up to my fork
