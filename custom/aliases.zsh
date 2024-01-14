2f07e68ff81537ed25a10985881ea239fba81b16
Sam Barker <sbarker@redhat.com>
2024-01-15 09:11:59 +1300

    swithch back to the prior branch
    rh-pre-commit.version: 2.0.1
    rh-pre-commit.check-secrets: ENABLED

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -3,6 +3,6 @@ alias dropMerged='git branch --merged | egrep -v "(^\*|master|main|dev)" | xargs
 alias bundleUpdate='antibody bundle < ${MY_PROFILE:-$HOME}/zsh_plugins.txt > ${MY_PROFILE:-$HOME}/zsh_plugins.zsh'
 alias gls='gls --hyperlink=auto --color=tty'
 alias mci='mvn clean install'
-alias updateFork='gco main && grhh && gf upstream && grb upstream/main && gpf!' #checkout main which tracks origin/main make sure its reset. Fetch & rebase to the latest upstream/main and push that up to my fork
+alias updateFork='BRANCH_NAME=${$(git symbolic-ref -q HEAD)##refs/heads/}; gco main && grhh && gf upstream && grb upstream/main && gpf! && gco ${BRANCH_NAME}' #checkout main which tracks origin/main make sure its reset. Fetch & rebase to the latest upstream/main and push that up to my fork
 
 source "${HOME}/.config/op/plugins.sh"
