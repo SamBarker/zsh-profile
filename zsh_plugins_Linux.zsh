5f649d806ca38011099ec6dbd8bf3584e14206d1
Sam Barker <sam@quadrocket.co.uk>
2024-01-30 15:17:47 +1300

    make sure SDKMan is initalised

--- zsh_plugins_Linux.zsh
+++ zsh_plugins_Linux.zsh
@@ -1,49 +1,48 @@
-source /home/sam/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-caarlos0-SLASH-jvm/jvm.plugin.zsh
-fpath+=( /home/sam/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-caarlos0-SLASH-jvm )
-source /home/sam/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-djui-SLASH-alias-tips/alias-tips.plugin.zsh
-fpath+=( /home/sam/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-djui-SLASH-alias-tips )
-source /home/sam/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-caarlos0-SLASH-zsh-mkc/mkc.plugin.zsh
-fpath+=( /home/sam/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-caarlos0-SLASH-zsh-mkc )
-source /home/sam/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-powerlevel10k/powerlevel10k.zsh-theme
-source /home/sam/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-powerlevel10k/powerlevel9k.zsh-theme
-fpath+=( /home/sam/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-powerlevel10k )
-source /home/sam/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
-fpath+=( /home/sam/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions )
-
-source /home/sam/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
-fpath+=( /home/sam/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
-source /home/sam/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
-fpath+=( /home/sam/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting )
-source /home/sam/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search/zsh-history-substring-search.plugin.zsh
-fpath+=( /home/sam/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search )
-
-
-source /home/sam/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-peterhurford-SLASH-git-it-on.zsh/git-it-on.plugin.zsh
-fpath+=( /home/sam/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-peterhurford-SLASH-git-it-on.zsh )
-source /home/sam/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-hschne-SLASH-fzf-git/fzf-git.plugin.zsh
-fpath+=( /home/sam/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-hschne-SLASH-fzf-git )
-source /home/sam/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/oh-my-zsh.sh
-fpath+=( /home/sam/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh )
-source /home/sam/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/git/git.plugin.zsh
-fpath+=( /home/sam/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/git )
-source /home/sam/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/keychain/keychain.plugin.zsh
-fpath+=( /home/sam/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/keychain )
-source /home/sam/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/gpg-agent/gpg-agent.plugin.zsh
-fpath+=( /home/sam/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/gpg-agent )
-source /home/sam/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/sdk/sdk.plugin.zsh
-fpath+=( /home/sam/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/sdk )
-source /home/sam/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/aws/aws.plugin.zsh
-fpath+=( /home/sam/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/aws )
-source /home/sam/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/kubectl/kubectl.plugin.zsh
-fpath+=( /home/sam/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/kubectl )
-source /home/sam/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/brew/brew.plugin.zsh
-fpath+=( /home/sam/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/brew )
-source /home/sam/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/minikube/minikube.plugin.zsh
-fpath+=( /home/sam/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/minikube )
-source /home/sam/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-mroth-SLASH-evalcache/evalcache.plugin.zsh
-fpath+=( /home/sam/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-mroth-SLASH-evalcache )
-source /home/sam/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-darvid-SLASH-zsh-poetry/poetry.plugin.zsh
-source /home/sam/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-darvid-SLASH-zsh-poetry/zsh-poetry.plugin.zsh
-fpath+=( /home/sam/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-darvid-SLASH-zsh-poetry )
-source /home/sam/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-matthieusb-SLASH-zsh-sdkman/zsh-sdkman.plugin.zsh
-fpath+=( /home/sam/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-matthieusb-SLASH-zsh-sdkman )
+fpath+=( /home/sam/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-caarlos0-SLASH-jvm )
+source /home/sam/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-caarlos0-SLASH-jvm/jvm.plugin.zsh
+fpath+=( /home/sam/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-djui-SLASH-alias-tips )
+source /home/sam/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-djui-SLASH-alias-tips/alias-tips.plugin.zsh
+fpath+=( /home/sam/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-caarlos0-SLASH-zsh-mkc )
+source /home/sam/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-caarlos0-SLASH-zsh-mkc/mkc.plugin.zsh
+fpath+=( /home/sam/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-powerlevel10k )
+source /home/sam/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-powerlevel10k/powerlevel10k.zsh-theme
+source /home/sam/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-powerlevel10k/powerlevel9k.zsh-theme
+fpath+=( /home/sam/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions )
+source /home/sam/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
+fpath+=( /home/sam/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
+source /home/sam/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+fpath+=( /home/sam/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting )
+source /home/sam/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
+fpath+=( /home/sam/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search )
+source /home/sam/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search/zsh-history-substring-search.plugin.zsh
+fpath+=( /home/sam/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-peterhurford-SLASH-git-it-on.zsh )
+source /home/sam/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-peterhurford-SLASH-git-it-on.zsh/git-it-on.plugin.zsh
+fpath+=( /home/sam/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-hschne-SLASH-fzf-git )
+source /home/sam/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-hschne-SLASH-fzf-git/fzf-git.plugin.zsh
+fpath+=( /home/sam/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh )
+source /home/sam/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/oh-my-zsh.sh
+fpath+=( /home/sam/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/git )
+source /home/sam/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/git/git.plugin.zsh
+fpath+=( /home/sam/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/keychain )
+source /home/sam/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/keychain/keychain.plugin.zsh
+fpath+=( /home/sam/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/gpg-agent )
+source /home/sam/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/gpg-agent/gpg-agent.plugin.zsh
+fpath+=( /home/sam/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/sdk )
+source /home/sam/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/sdk/sdk.plugin.zsh
+fpath+=( /home/sam/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/aws )
+source /home/sam/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/aws/aws.plugin.zsh
+fpath+=( /home/sam/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/kubectl )
+source /home/sam/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/kubectl/kubectl.plugin.zsh
+fpath+=( /home/sam/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/brew )
+source /home/sam/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/brew/brew.plugin.zsh
+fpath+=( /home/sam/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/minikube )
+source /home/sam/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/minikube/minikube.plugin.zsh
+fpath+=( /home/sam/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/pyenv )
+source /home/sam/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/pyenv/pyenv.plugin.zsh
+fpath+=( /home/sam/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-mroth-SLASH-evalcache )
+source /home/sam/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-mroth-SLASH-evalcache/evalcache.plugin.zsh
+fpath+=( /home/sam/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-darvid-SLASH-zsh-poetry )
+source /home/sam/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-darvid-SLASH-zsh-poetry/poetry.plugin.zsh
+source /home/sam/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-darvid-SLASH-zsh-poetry/zsh-poetry.plugin.zsh
+fpath+=( /home/sam/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-matthieusb-SLASH-zsh-sdkman )
+source /home/sam/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-matthieusb-SLASH-zsh-sdkman/zsh-sdkman.plugin.zsh
--- zshrc
+++ zshrc
@@ -150,3 +150,7 @@ export DOCKER_ORG=${QUAY_ORG}
 export DOCKER_REGISTRY=quay.io
 export CONTAINER_ENGINE=podman
 export MY_PROFILE
+
+#THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
+export SDKMAN_DIR="$HOME/.sdkman"
+[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
